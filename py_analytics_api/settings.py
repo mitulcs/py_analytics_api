4c0229600da3b65ad31b512741ff394026a39c0d
Mitul Nakum <63703973+mitulcs@users.noreply.github.com>
2022-10-04 15:03:33 +0530

    Update settings.py

--- py_analytics_api/settings.py
+++ py_analytics_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1ow4$3(r5ln#_4baon4b!l6ge4o&&v&@(knezo5(9gljh3km#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['py-analytics-api.herokuapp.com']
+ALLOWED_HOSTS = ['py-analytics-api.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
